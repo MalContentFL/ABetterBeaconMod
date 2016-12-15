bdc0b31a298f068c807f65380b935a6859ec4516
MalContentFL <MalContentFL@gmail.com>
2016-12-14 21:13:31 -0500

    Not Much

--- src/main/java/malcontentfl/abetterbeaconmod/blocks/BlockEndowingTable.java
+++ src/main/java/malcontentfl/abetterbeaconmod/blocks/BlockEndowingTable.java
@@ -6,10 +6,16 @@ import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-//public class BlockEndowingTable extends Block implements ITileEntityProvider
-//{
-//	public BlockEndowingTable()
-//	{
-//		super(Material.ROCK);
-//	}
-//}
+public class BlockEndowingTable extends Block implements ITileEntityProvider
+{
+	public BlockEndowingTable()
+	{
+		super(Material.ROCK);
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta)
+	{
+		return null;
+	}
+}
